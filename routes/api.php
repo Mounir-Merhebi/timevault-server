63d214dd28d1cf5d46cd371c9cace5c68c6c05e4
MounirPrograms <mounirmerhebi21@gmail.com>
2025-07-17 21:03:22 +0300

    added routes

--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Users\ProfileController;
-use App\Http\Controllers\Users\CreateCapsuleController;
-use App\Http\Controllers\Users\DashboardController;
-use App\Http\Controllers\Users\LoginController;
-use App\Http\Controllers\Users\SignupController;
-use App\Http\Controllers\Users\PublicWallController;
-use App\Http\Controllers\Users\ViewCapsuleController;
+use App\Http\Controllers\User\ProfileController;
+use App\Http\Controllers\User\CreateCapsuleController;
+use App\Http\Controllers\User\DashboardController;
+use App\Http\Controllers\User\LoginController;
+use App\Http\Controllers\User\SignupController;
+use App\Http\Controllers\User\PublicWallController;
+use App\Http\Controllers\User\ViewCapsuleController;
 
-Route::get('/profile', [ProfileController::class, 'test4']);
+Route::get('/Profile/{id}', [ProfileController::class, 'getUser']);
 Route::get('/CreateCapsule', [CreateCapsuleController::class, 'test1']);
-Route::get('/Dashboard', [DashboardController::class, 'test2']);
+Route::get('/PublicWallAllCount', [PublicWallController::class, 'getAllCount']);
 Route::get('/Login', [LoginController::class, 'test3']);
 Route::get('/Signup', [SignupController::class, 'test6']);
-Route::get('/PublicWall', [PublicWallController::class, 'test5']);
-Route::get('/ViewCapsule', [ViewCapsuleController::class, 'test7']);
+Route::get('/PublicWallCapsules', [PublicWallController::class, 'getAllCapsules']);
+Route::get('/ViewCapsule/{id}', [ViewCapsuleController::class, 'getCapsule']);
+Route::get('/Dashboard/{id}', [DashboardController::class, 'getUserCapsules']);
+Route::get('/DashboardTotalCount/{id}', [DashboardController::class, 'getTotalCount']);
+Route::get('/DashboardWaitingCount/{id}', [DashboardController::class, 'getWaitingCount']);
+Route::get('/DashboardPublicCount/{id}', [DashboardController::class, 'getPublicCount']);
 
