3a9744e80e630ddd12736d9db5426b351d4f3e82
MounirPrograms <mounirmerhebi21@gmail.com>
2025-07-17 21:49:02 +0300

    added versioning to routes

--- routes/api.php
+++ routes/api.php
@@ -10,15 +10,19 @@ use App\Http\Controllers\User\SignupController;
 use App\Http\Controllers\User\PublicWallController;
 use App\Http\Controllers\User\ViewCapsuleController;
 
-Route::get('/Profile/{id}', [ProfileController::class, 'getUser']);
-Route::get('/CreateCapsule', [CreateCapsuleController::class, 'test1']);
-Route::get('/PublicWallAllCount', [PublicWallController::class, 'getAllCount']);
-Route::get('/Login', [LoginController::class, 'test3']);
-Route::get('/Signup', [SignupController::class, 'test6']);
-Route::get('/PublicWallCapsules', [PublicWallController::class, 'getAllCapsules']);
-Route::get('/ViewCapsule/{id}', [ViewCapsuleController::class, 'getCapsule']);
-Route::get('/Dashboard/{id}', [DashboardController::class, 'getUserCapsules']);
-Route::get('/DashboardTotalCount/{id}', [DashboardController::class, 'getTotalCount']);
-Route::get('/DashboardWaitingCount/{id}', [DashboardController::class, 'getWaitingCount']);
-Route::get('/DashboardPublicCount/{id}', [DashboardController::class, 'getPublicCount']);
 
+Route::group(["prefix" =>"v0.1"], function(){
+    Route::group(["prefix" => "user"], function(){
+        Route::get('/Profile/{id}', [ProfileController::class, 'getUser']);
+        Route::get('/CreateCapsule', [CreateCapsuleController::class, 'test1']);
+        Route::get('/PublicWallAllCount', [PublicWallController::class, 'getAllCount']);
+        Route::get('/Login', [LoginController::class, 'test3']);
+        Route::get('/Signup', [SignupController::class, 'test6']);
+        Route::get('/PublicWallCapsules', [PublicWallController::class, 'getAllCapsules']);
+        Route::get('/ViewCapsule/{id}', [ViewCapsuleController::class, 'getCapsule']);
+        Route::get('/Dashboard/{id}', [DashboardController::class, 'getUserCapsules']);
+        Route::get('/DashboardTotalCount/{id}', [DashboardController::class, 'getTotalCount']);
+        Route::get('/DashboardWaitingCount/{id}', [DashboardController::class, 'getWaitingCount']);
+        Route::get('/DashboardPublicCount/{id}', [DashboardController::class, 'getPublicCount']);
+    });
+});
